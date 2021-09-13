50eb88c8687e6bc8a1144aa2bfe6eb0d05c414f1
gearsix <gearsix@tuta.io>
2021-09-13 13:11:57 +0100

    first release tidyup
    - renamed to "htrack"
    - renamed functions accordingly
    - minor bugfixes to heapsiz tracking
    - added comment documentation
    - added TODO.txt

---
+++ htrack.h
@@ -0,0 +1,36 @@
+/**
+ * See README.md for documentation
+**/
+
+#ifndef STDLIBW
+#define STDLIBW
+
+#include <stdlib.h>
+#include <assert.h>
+
+/* num. bytes heap-allocated by htrack */
+size_t htrack_siz();
+
+/* the max. number of heap-allocated bytes htrack will allow */
+size_t htrack_limit();
+
+size_t htrack_setlimit(size_t n);
+
+/* The number of bytes allocated for `ptr`.
+ * ptr MUST be allocated by htrack */
+size_t htrack_allocsiz(void *ptr);
+
+/**
+ * stdlib wrappers - behave the same as their stdlib counterparts
+**/
+void *htrack_malloc(size_t n);
+
+void *htrack_calloc(size_t num, size_t n);
+
+/* ptr MUST be allocated by htrack */
+void *htrack_realloc(void *ptr, size_t n);
+
+/* ptr MUST be allocated by htrack */
+void htrack_free(void *ptr);
+
+#endif
